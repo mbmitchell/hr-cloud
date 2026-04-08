bacc67ffbd075a02900b8f8a00ff031dbf0f02b0
Marty Mitchell <orionbrock32@gmail.com>
2026-04-08 13:51:48 -0500

    Fix onboarding acknowledgements migration

--- prisma/migrations/20260408163526_add_onboarding_acknowledgements/migration.sql
+++ prisma/migrations/20260408163526_add_onboarding_acknowledgements/migration.sql
@@ -1,23 +1,4 @@
--- DropForeignKey
-ALTER TABLE `EmployeeDocumentAssignment` DROP FOREIGN KEY `EmployeeDocumentAssignment_sourceEmployeeOnboardingTa_fkey`;
-
--- DropForeignKey
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` DROP FOREIGN KEY `EmployeeOnboardingTaskAcknowledgementRequirement_ass_2_fkey`;
-
--- DropForeignKey
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` DROP FOREIGN KEY `EmployeeOnboardingTaskAcknowledgementRequirement_assi_fkey`;
-
--- DropForeignKey
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` DROP FOREIGN KEY `EmployeeOnboardingTaskAcknowledgementRequirement_empl_2_fkey`;
-
--- DropForeignKey
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` DROP FOREIGN KEY `EmployeeOnboardingTaskAcknowledgementRequirement_empl_fkey`;
-
--- DropForeignKey
-ALTER TABLE `OnboardingTemplateTaskAcknowledgementRequirement` DROP FOREIGN KEY `OnboardingTemplateTaskAcknowledgementRequirement_assi_fkey`;
-
--- DropForeignKey
-ALTER TABLE `OnboardingTemplateTaskAcknowledgementRequirement` DROP FOREIGN KEY `OnboardingTemplateTaskAcknowledgementRequirement_temp_fkey`;
+-
 
 -- AddForeignKey
 ALTER TABLE `EmployeeDocumentAssignment` ADD CONSTRAINT `EmployeeDocumentAssignment_sourceEmployeeOnboardingTaskRequ_fkey` FOREIGN KEY (`sourceEmployeeOnboardingTaskRequirementId`) REFERENCES `EmployeeOnboardingTaskAcknowledgementRequirement`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
@@ -40,23 +21,3 @@ ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` ADD CONSTRAINT `E
 -- AddForeignKey
 ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` ADD CONSTRAINT `EmployeeOnboardingTaskAcknowledgementRequirement_employeeDo_fkey` FOREIGN KEY (`employeeDocumentAssignmentId`) REFERENCES `EmployeeDocumentAssignment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
--- RenameIndex
-ALTER TABLE `EmployeeDocumentAssignment` RENAME INDEX `EmployeeDocumentAssignment_sourceEmployeeOnboardingTaskR_idx` TO `EmployeeDocumentAssignment_sourceEmployeeOnboardingTaskRequi_idx`;
-
--- RenameIndex
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` RENAME INDEX `EmployeeOnboardingTaskAcknowledgementRequirement_ass_2_idx` TO `EmployeeOnboardingTaskAcknowledgementRequirement_assignedDoc_idx`;
-
--- RenameIndex
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` RENAME INDEX `EmployeeOnboardingTaskAcknowledgementRequirement_assig_idx` TO `EmployeeOnboardingTaskAcknowledgementRequirement_assignableD_idx`;
-
--- RenameIndex
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` RENAME INDEX `EmployeeOnboardingTaskAcknowledgementRequirement_empl_2_idx` TO `EmployeeOnboardingTaskAcknowledgementRequirement_employeeDoc_idx`;
-
--- RenameIndex
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` RENAME INDEX `EmployeeOnboardingTaskAcknowledgementRequirement_emplo_idx` TO `EmployeeOnboardingTaskAcknowledgementRequirement_employeeOnb_idx`;
-
--- RenameIndex
-ALTER TABLE `OnboardingTemplateTaskAcknowledgementRequirement` RENAME INDEX `OnboardingTemplateTaskAcknowledgementRequirement_assignabl_idx` TO `OnboardingTemplateTaskAcknowledgementRequirement_assignableD_idx`;
-
--- RenameIndex
-ALTER TABLE `OnboardingTemplateTaskAcknowledgementRequirement` RENAME INDEX `OnboardingTemplateTaskAcknowledgementRequirement_template_idx` TO `OnboardingTemplateTaskAcknowledgementRequirement_templateTas_idx`;
