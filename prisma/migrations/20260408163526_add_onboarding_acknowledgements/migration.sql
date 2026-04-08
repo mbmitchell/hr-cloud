8ea94e734efa5e48b5f83882f5976ea3b3fdcf05
Marty Mitchell <orionbrock32@gmail.com>
2026-04-08 14:02:15 -0500

    Fix onboarding acknowledgements migration

--- prisma/migrations/20260408163526_add_onboarding_acknowledgements/migration.sql
+++ prisma/migrations/20260408163526_add_onboarding_acknowledgements/migration.sql
@@ -1,21 +1,2 @@
--- AddForeignKey
-ALTER TABLE `EmployeeDocumentAssignment` ADD CONSTRAINT `EmployeeDocumentAssignment_sourceEmployeeOnboardingTaskRequ_fkey` FOREIGN KEY (`sourceEmployeeOnboardingTaskRequirementId`) REFERENCES `EmployeeOnboardingTaskAcknowledgementRequirement`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `OnboardingTemplateTaskAcknowledgementRequirement` ADD CONSTRAINT `OnboardingTemplateTaskAcknowledgementRequirement_templateTa_fkey` FOREIGN KEY (`templateTaskId`) REFERENCES `OnboardingTemplateTask`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `OnboardingTemplateTaskAcknowledgementRequirement` ADD CONSTRAINT `OnboardingTemplateTaskAcknowledgementRequirement_assignable_fkey` FOREIGN KEY (`assignableDocumentId`) REFERENCES `AssignableDocument`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` ADD CONSTRAINT `EmployeeOnboardingTaskAcknowledgementRequirement_employeeOn_fkey` FOREIGN KEY (`employeeOnboardingTaskId`) REFERENCES `EmployeeOnboardingTask`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` ADD CONSTRAINT `EmployeeOnboardingTaskAcknowledgementRequirement_assignable_fkey` FOREIGN KEY (`assignableDocumentId`) REFERENCES `AssignableDocument`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` ADD CONSTRAINT `EmployeeOnboardingTaskAcknowledgementRequirement_assignedDo_fkey` FOREIGN KEY (`assignedDocumentVersionId`) REFERENCES `AssignableDocumentVersion`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `EmployeeOnboardingTaskAcknowledgementRequirement` ADD CONSTRAINT `EmployeeOnboardingTaskAcknowledgementRequirement_employeeDo_fkey` FOREIGN KEY (`employeeDocumentAssignmentId`) REFERENCES `EmployeeDocumentAssignment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
+--Intentionally left blank
+--Supeseeded by later acknowledgement migrations.
